5d79c8a4d22e9247fc5a42990b52860fbb1cbdd7
fiep-hcm-automation <heber.silva@senior.com.br>
2018-08-22 16:23:14 -0300

    teste permissão

---
+++ gestao-pessoas/src/senior/gep/utils/aaaa.java
@@ -0,0 +1,18 @@
+package senior.gep.utils;
+
+/**
+ *         Classe destinada ao armazenamento de usuários utilizados pelos testes.
+ */
+public class aaaa {
+
+    /**
+     * Usuário destinado aos testes do eSocial.
+     */
+    public static final String ESOCIAL = "-s:senior/senior";
+    
+    /**
+     * Usuário destinado aos testes do HCM.
+     */
+    public static final String HCM = "-s:senior/senior";
+
+}
